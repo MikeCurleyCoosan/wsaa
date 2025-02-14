e87c5028d62335c7a28e681b4ae3ff2e577f2b04
Michael Curley <michaelcurley1@gmail.com>
2025-02-14 00:00:40 +0000

    Adding dictionary.py to week3_work, exploring API's using json

--- mywork/week2_work/lab2.py
+++ mywork/week2_work/lab2.py
@@ -18,24 +18,34 @@ doc = parseString(page.content)
 
 #print (doc.toprettyxml(newl='')) #output to console
 
+#To store the xml in a file. 
+#with open("trains.xml", "w") as f:
+   # doc.writexml(f, newl='\n')
 
-objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
+with open ('week03_train.csv', mode='w', newline='') as train_file:
+    train_writer = csv.writer(train_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-for objTrainPositionNode in objTrainPositionsNodes:
-    traincodenode = objTrainPositionNode.getElementsByTagName("TrainCode").item(0)
-    traincode = traincodenode.firstChild.nodeValue.strip()
+
+    objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
+
+    for objTrainPositionNode in objTrainPositionsNodes:
+        traincodenode = objTrainPositionNode.getElementsByTagName("TrainCode").item(0)
+        traincode = traincodenode.firstChild.nodeValue.strip()
+        list = []
+        list.append(traincode)
+        train_writer.writerow(list)
     #print(traincode)
 
-    # now lets get everything
-    dataList = []
-    flag = False
-    for retrieveTag in retrieveTags:
-        datanode = objTrainPositionNode.getElementsByTagName(retrieveTag).item(0)
-        print(f"Train Object tags are {retrieveTag}")
-        dataList.append(datanode.firstChild.nodeValue.strip())
-        # this print shows each line being incrementally built
-        print(f"{dataList}")
-        # only store train codes beginning with 'D'
-        if retrieveTag == 'TrainCode' and datanode.firstChild.nodeValue[0] == 'D':
-            flag = True
+# now lets get everything
+dataList = []
+flag = False
+for retrieveTag in retrieveTags:
+    datanode = objTrainPositionNode.getElementsByTagName(retrieveTag).item(0)
+    print(f"Train Object tags are {retrieveTag}")
+    dataList.append(datanode.firstChild.nodeValue.strip())
+    # this print shows each line being incrementally built
+    print(f"{dataList}")
+    # only store train codes beginning with 'D'
+    if retrieveTag == 'TrainCode' and datanode.firstChild.nodeValue[0] == 'D':
+        flag = True
     
