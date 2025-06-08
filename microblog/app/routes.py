5aebd6d5de9d50fd1c84e438e511e537472b6b55
Michael Curley <118756697+MikeCurleyCoosan@users.noreply.github.com>
2025-06-08 20:12:00 +0000

    The Flask Mega Tutorial, chapter 3 update

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,13 +1,20 @@
 from flask import render_template 
 from app import app 
+from app.forms import LoginForm
 
 @app.route('/') 
 @app.route('/index') 
 def index(): 
-    user = {'username': 'Michael'} 
+    user = {'username': 'Wayne McCoy'} 
     posts = [ 
-        { 'author': {'username': 'John'}, 'body': 'Beautiful day in Portland!' }, 
-        { 'author': {'username': 'Susan'}, 'body': 'The Avengers movie was so cool!' },
-        { 'author': {'username': 'Mike'}, 'body': 'This is the last post'} 
+        { 'author': {'username': 'Geo'}, 'body': 'We have flowmarks' }, 
+        { 'author': {'username': 'Keith'}, 'body': 'The Avengers movie was so cool!' },
+        { 'author': {'username': 'Mike'}, 'body': 'Get the fucking line running'} 
     ] 
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
